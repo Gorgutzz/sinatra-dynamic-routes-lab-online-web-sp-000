3c5df6f738edb9c8916e32c29c59072a955c0f7f
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-06-17 02:02:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,5 +28,19 @@ class App < Sinatra::Base
     @word_string = "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
 
+  get '/:operation/:number1/:number2' do 
+    case params[:operation]
+    when "add"
+      @result = params[:number1].to_i + params[:number2].to_i
+    when "subtract"
+      @result = params[:number1].to_i - params[:number2].to_i
+    when "multiply"
+      @result = params[:number1].to_i * params[:number2].to_i
+    when "divide"
+      @result = params[:number1].to_i / params[:number2].to_i
+    end 
+    "#{@result}"
+  end 
+
 
 end
